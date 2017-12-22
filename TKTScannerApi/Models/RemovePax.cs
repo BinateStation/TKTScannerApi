86e6ef7e4ce926935eeb9d80283aefe63f47a11b
Raghu krishnan R <raghukrishnan@cmmtechnologies.in>
2017-12-22 19:54:39 +0530

    Build version 1.0.0

---
+++ TKTScannerApi/Models/RemovePax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiApp.Models
+{
+    public class RemovePax
+    {
+        public bool status { get; set; }
+        public Pax pax { get; set; }
+    }
+   
+
+   
+}
